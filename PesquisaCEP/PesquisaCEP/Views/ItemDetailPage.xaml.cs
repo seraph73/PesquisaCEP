8257582c20294f10becbbea7104cb4f56e7de690
thayleon <gblfernandes73@gmail.com>
2020-09-21 00:53:08 -0300

    Adicionar arquivos de projeto.

---
+++ PesquisaCEP/PesquisaCEP/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using PesquisaCEP.ViewModels;
+
+namespace PesquisaCEP.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ PesquisaCEP/PesquisaCEP/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using PesquisaCEP.Models;
+using PesquisaCEP.ViewModels;
+
+namespace PesquisaCEP.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
