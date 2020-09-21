e5875605a3ed200898a0bef65e71902eb5c86048
thayleon <gblfernandes73@gmail.com>
2020-09-21 02:10:36 -0300

    Revert "Revert "Foi criado um Behaviour para a classe Entry de forma que as entradas emitidas pelo usuário sejam apenas numéricas.""
    This reverts commit 45be1c701253e3a769c5a178363080522cabdbf6.

---
+++ PesquisaCEP/PesquisaCEP/Behaviours/ApenasNumeros.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PesquisaCEP.Behaviours
+{
+    public class ApenasNumeros : Behavior<Entry>
+    {
+        /// <summary>
+        /// Check about value entered in the entry and convert it
+        /// </summary>
+        protected Action<Entry, string> AdditionalCheck;
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            bindable.TextChanged += TextChanged_Handler;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+        }
+
+        protected virtual void TextChanged_Handler(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(e.NewTextValue))
+            {
+                (sender as Entry).Text = "";
+                return;
+            }
+
+            double _;
+            if (!double.TryParse(e.NewTextValue, out _) || e.NewTextValue[e.NewTextValue.Length - 1] == '.')
+                (sender as Entry).Text = e.OldTextValue;
+            else
+                AdditionalCheck?.Invoke(((Entry)sender), e.OldTextValue);
+        }
+    }
+}
