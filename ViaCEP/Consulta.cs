09c6d79876d1e6ecca042c4cb3df94b7707a5862
thayleon <gblfernandes73@gmail.com>
2020-09-23 12:51:05 -0300

    Projeto VIA CEP foi recolocado pois o git não o havia reconhecido.

---
+++ ViaCEP/Consulta.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ViaCEP
+{
+    public class Consulta
+    {
+
+        /// <summary>
+        /// URL do site ViaCEP
+        /// </summary>
+        private const string UrlViaCEP = "https://viacep.com.br";
+        
+        /// <summary>
+        /// Cliente HTTP.
+        /// </summary>
+        private readonly HttpClient _clienteHTTP;
+
+        public Consulta()
+        {
+            _clienteHTTP = HttpClientFactory.Create();
+            _clienteHTTP.BaseAddress = new Uri(UrlViaCEP);
+        }
+
+        public EnderecoCompleto Buscar(string zipCode)
+        {
+            return BuscarAssincronamente(zipCode, CancellationToken.None).Result;
+        }
+
+        private async Task<EnderecoCompleto> BuscarAssincronamente(string zipCode, CancellationToken cancellationToken)
+        {
+            var Resposta = await _clienteHTTP.GetAsync($"/ws/{zipCode}/json", cancellationToken).ConfigureAwait(false);
+            
+
+            Resposta.EnsureSuccessStatusCode();
+            return await Resposta.Content.ReadAsAsync<EnderecoCompleto>(cancellationToken).ConfigureAwait(false);
+
+        }
+    }
+}
---
+++ ViaCEP/EnderecoCompleto.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ViaCEP
+{
+    /// <summary>
+    /// Esta classe encapsula o resultado das consultas de CEP, 
+    /// de forma que todas as informações sejam contidas em uma
+    /// instância da mesma.
+    /// </summary>
+    public class EnderecoCompleto
+    {
+        /// <summary>
+        /// Recupera ou Insere o número do CEP.
+        /// </summary>
+        [JsonProperty("cep")]
+        public string CEP { get; set; }
+
+        /// <summary>
+        /// Recupera ou Insere o nome da Rua.
+        /// </summary>
+        [JsonProperty("logradouro")]
+        public string Rua { get; set; }
+
+        /// <summary>
+        /// Recupera ou Insere um Complemento.
+        /// </summary>
+        [JsonProperty("complemento")]
+        public string Complemento { get; set; }
+
+        /// <summary>
+        /// Recupera ou Insere o nome do Bairro.
+        /// </summary>
+        [JsonProperty("bairro")]
+        public string Bairro { get; set; }
+
+        /// <summary>
+        /// Recupera ou Insere o nome da Cidade.
+        /// </summary>
+        [JsonProperty("localidade")]
+        public string Cidade { get; set; }
+
+        /// <summary>
+        /// Recupera ou Insere as iniciais do Estado (Unidade Federativa).
+        /// </summary>
+        [JsonProperty("uf")]
+        public string Estado { get; set; }
+
+        /// <summary>
+        /// Recupera ou Insere o código do IBGE.
+        /// </summary>
+        [JsonProperty("ibge")]
+        public int CodigoIBGE { get; set; }
+
+        /// <summary>
+        /// Recupera ou Insere o código de GIA.
+        /// </summary>
+        [JsonProperty("gia")]
+        public int? CodigoGIA { get; set; }
+
+        /// <summary>
+        /// Recupera ou Insere o DDD.
+        /// </summary>
+        [JsonProperty("ddd")]
+        public int DDD { get; set; }
+
+        /// <summary>
+        /// Recupera ou Insere o código de SIAFI.
+        /// </summary>
+        [JsonProperty("siafi")]
+        public int? CodigoSIAFI { get; set; }
+
+        public override string ToString()
+        {
+            return $"CEP: {CEP}{Environment.NewLine}" +
+                $"Rua: {Rua}{Environment.NewLine}" +
+                $"Complemento: {Complemento}{Environment.NewLine}" +
+                $"Bairro: {Bairro}{Environment.NewLine}" +
+                $"Cidade: {Cidade}{Environment.NewLine}" +
+                $"Estado: {Estado}{Environment.NewLine}" +
+                $"Código do IBGE: {CodigoIBGE}{Environment.NewLine}" +
+                $"Código do GIA: {CodigoGIA}{Environment.NewLine}" +
+                $"Código do SIAFI: {CodigoSIAFI}{Environment.NewLine}" +
+                $"DDD: {DDD}";
+        }
+    }
+}
